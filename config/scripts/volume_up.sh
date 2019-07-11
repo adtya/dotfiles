14a0378fb3f3ae2f0d3bbb8c10e6827467ca8373
Adithya Nair <adtya.nair@gmail.com>
2019-07-11 19:25:59 +0530

    update volume change scripts with more ideal values

--- config/scripts/volume_up.sh
+++ config/scripts/volume_up.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 CURR_VOL="$(pactl list sinks | grep '^\s*Volume' | awk '{print $5}' | sed 's/\%//')"
-if [ $CURR_VOL -lt 100 ] ; then
+if [ $CURR_VOL -lt 110 ] ; then
     pactl set-sink-volume alsa_output.pci-0000_00_1f.3.analog-stereo +5%
 fi
 CURR_VOL="$(pactl list sinks | grep '^\s*Volume' | awk '{print $5}' | sed 's/\%//')"
-if [ $CURR_VOL -gt 100 ] ; then
-    pactl set-sink-volume alsa_output.pci-0000_00_1f.3.analog-stereo 100%
+if [ $CURR_VOL -gt 110 ] ; then
+    pactl set-sink-volume alsa_output.pci-0000_00_1f.3.analog-stereo 110%
 fi
