a69d12df38c41b93bb242564f3f1f38fbe13e57e
Adithya Nair <mail@adtya.xyz>
2020-01-30 18:24:37 +0530

    different images on different monitors

--- config/scripts/chpaper.sh
+++ config/scripts/chpaper.sh
@@ -3,5 +3,12 @@
 DIR="${HOME}/Pictures/Wallpapers"
 
 RANDOM_PAPER="$(/bin/ls ${DIR}/*/* | /usr/bin/shuf -n 1)"
-swaymsg "output * bg '${RANDOM_PAPER}' fill"
-echo "\"${RANDOM_PAPER}\"" > /tmp/wallpaper
+
+random_paper() {
+	/usr/bin/find ${DIR}/ -type f -regextype egrep -regex ".*\.(jpe?g|png)$" | /usr/bin/shuf -n1
+}
+
+[ -z "$SWAYSOCK" ] && export SWAYSOCK="$(/usr/bin/find /run/user/${UID}/ -name "sway-ipc.${UID}.*.sock")"
+/usr/bin/swaymsg "output eDP-1 bg '$(random_paper)' fill"
+/usr/bin/swaymsg "output HDMI-A-1 bg '$(random_paper)' fill"
+echo "\"$(random_paper)\"" > /tmp/wallpaper
