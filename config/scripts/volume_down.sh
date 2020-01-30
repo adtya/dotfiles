667c606f9d65dd77bfcb2a44f326b1e38e74d025
Adithya Nair <mail@adtya.xyz>
2020-01-30 18:25:06 +0530

    fix bug when there are multiple audio sinks connected

--- config/scripts/volume_down.sh
+++ config/scripts/volume_down.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-CURR_VOL="$(pactl list sinks | grep '^\s*Volume' | awk '{print $5}' | sed 's/\%//' | head -n1)"
+CURR_VOL="$(pactl list sinks | grep '^\s*Volume' | awk '{print $5}' | sed 's/\%//' | tail -n1)"
 if [ $CURR_VOL -le 5 ] ; then
 	pactl set-sink-volume @DEFAULT_SINK@ 0
 	pactl set-sink-mute @DEFAULT_SINK@ 1
