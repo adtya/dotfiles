0ce936aa254079b7d5b1b73311bdf4575ea37fad
Adithya Nair <adtya.nair@gmail.com>
2020-02-22 17:30:08 +0530

    copy wallpaper over to /tmp for faster access

--- config/scripts/chpaper.sh
+++ config/scripts/chpaper.sh
@@ -2,13 +2,10 @@
 
 DIR="${HOME}/Pictures/Wallpapers"
 
-RANDOM_PAPER="$(/bin/ls ${DIR}/*/* | /usr/bin/shuf -n 1)"
-
 random_paper() {
 	/usr/bin/find ${DIR}/ -type f -regextype egrep -regex ".*\.(jpe?g|png)$" | /usr/bin/shuf -n1
 }
 
 [ -z "$SWAYSOCK" ] && export SWAYSOCK="$(/usr/bin/find /run/user/${UID}/ -name "sway-ipc.${UID}.*.sock")"
-/usr/bin/swaymsg "output eDP-1 bg '$(random_paper)' fill"
-/usr/bin/swaymsg "output HDMI-A-1 bg '$(random_paper)' fill"
-echo "\"$(random_paper)\"" > /tmp/wallpaper
+/usr/bin/magick convert "$(random_paper)" /tmp/wallpaper.jpg && /usr/bin/swaymsg "output * bg '/tmp/wallpaper.jpg' fill" &
+/usr/bin/magick convert "$(random_paper)" /tmp/lockpaper.jpg
