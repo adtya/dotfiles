167371d3b8d704c0eeeaa6e32305b4198d09884a
Adithya Nair <adtya.nair@gmail.com>
2020-02-22 17:32:51 +0530

    now volume can go upto 150%

--- config/scripts/volume_up.sh
+++ config/scripts/volume_up.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 pactl set-sink-mute @DEFAULT_SINK@ 0
 CURR_VOL="$(pactl list sinks | grep '^\s*Volume' | awk '{print $5}' | sed 's/\%//' | head -n1)"
-if [ $CURR_VOL -lt 110 ] ; then
+if [ $CURR_VOL -lt 150 ] ; then
     pactl set-sink-volume @DEFAULT_SINK@ +5%
 fi
 CURR_VOL="$(pactl list sinks | grep '^\s*Volume' | awk '{print $5}' | sed 's/\%//' | head -n1)"
-if [ $CURR_VOL -gt 110 ] ; then
-    pactl set-sink-volume @DEFAULT_SINK@ 110%
+if [ $CURR_VOL -gt 150 ] ; then
+    pactl set-sink-volume @DEFAULT_SINK@ 150%
 fi
