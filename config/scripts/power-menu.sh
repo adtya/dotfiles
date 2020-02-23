c2844915adabffeab4aa8d3fc86c8a35291a6dbd
Adithya Nair <adtya.nair@gmail.com>
2020-02-23 19:58:50 +0530

    make the script dmenu compatible

---
+++ config/scripts/power-menu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+chpower() {
+	case "$1" in
+		"")
+			notify-send -t 1500 -u low "Canceled"
+		;;
+		Shutdown)
+			exec sudo openrc-shutdown -p now
+		;;
+		Reboot)
+			exec sudo openrc-shutdown -r now
+		;;
+		*)
+			notify-send -t 1500 -u low "Invalid Option"
+		;;
+	esac
+}
+
+OPTIONS="Shutdown\nReboot"
+
+chpower "$(echo -e $OPTIONS | dmenu -p "Power Menu")"
