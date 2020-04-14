c72a2a810f8eecc939cfcb57262818fc8de24a91
Adithya Nair <adtya.nair@gmail.com>
2020-04-14 16:48:05 +0530

    remove dirname()

--- config/scripts/vim-sessions.sh
+++ config/scripts/vim-sessions.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -eu
 
-SESSION="$(find "${HOME}/Projects" -type f -name '.vimsession' -exec dirname {} \; | dmenu -p "Saved VIM Sessions")"
+SESSION="$(find "${HOME}/Projects" -type f -name '.vimsession' | dmenu -p "Saved VIM Sessions")"
 
-kitty vi -S "${SESSION}/.vimsession"
+kitty tmux new-session vi -S "${SESSION}"
