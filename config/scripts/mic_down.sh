66b464fcefcbd709b26121a679fc547019159b4d
Adithya Nair <adtya@adtya.xyz>
2022-01-31 00:08:25 +0530

    update volume scripts

--- config/scripts/mic_down.sh
+++ config/scripts/mic_down.sh
@@ -2,11 +2,5 @@
 
 set -eu
 
-CURR_VOL="$(pactl list sources | grep '^\s*Volume' | awk '{print $5}' | sed 's/\%//' | tail -1)"
-if [ "$CURR_VOL" -le 5 ] ; then
-	pactl set-source-volume @DEFAULT_SOURCE@ 0%
-	pactl set-source-mute @DEFAULT_SOURCE@ 1
-else
-	pactl set-source-volume @DEFAULT_SOURCE@ -5%
-fi
+pactl set-source-volume @DEFAULT_SOURCE@ -5%
 
--- config/scripts/volume_up.sh
+++ config/scripts/volume_up.sh
@@ -4,11 +4,11 @@ set -eu
 
 pactl set-sink-mute @DEFAULT_SINK@ 0
 CURR_VOL="$(pactl list sinks | grep '^\s*Volume' | awk '{print $5}' | sed 's/\%//' | tail -n1)"
-if [ "$CURR_VOL" -lt 150 ] ; then
+if [ "$CURR_VOL" -lt 100 ] ; then
     pactl set-sink-volume @DEFAULT_SINK@ +5%
 fi
 CURR_VOL="$(pactl list sinks | grep '^\s*Volume' | awk '{print $5}' | sed 's/\%//' | tail -n1)"
-if [ "$CURR_VOL" -gt 150 ] ; then
-    pactl set-sink-volume @DEFAULT_SINK@ 150%
+if [ "$CURR_VOL" -gt 100 ] ; then
+    pactl set-sink-volume @DEFAULT_SINK@ 100%
 fi
 
