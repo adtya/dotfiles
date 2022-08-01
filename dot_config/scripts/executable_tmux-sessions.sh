96ec640eb2a3b35f975ebefa1ae688ef4e3997f5
Adithya Nair <adtya@adtya.xyz>
2022-08-01 19:42:34 +0530

    Update dot_config/scripts/executable_tmux-sessions.sh

--- dot_config/scripts/executable_tmux-sessions.sh
+++ dot_config/scripts/executable_tmux-sessions.sh
@@ -3,7 +3,6 @@
 set -eu
 
 SESSION="$(tmux list-sessions -F "(#{session_attached}) #S [#{pane_current_command} in #{pane_current_path}] #{pane_title}" | sort | dmenu -p "Running TMUX Sessions" | awk '{print $2}')"
-
 case "$SESSION" in
 	"")
 		;;
