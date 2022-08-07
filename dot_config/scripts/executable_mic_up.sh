49b0af9a70919646b27aba8ae09e18ecd5fbcb8a
Adithya Nair <adtya@adtya.xyz>
2022-08-07 11:25:16 +0530

    Update dot_config/scripts/executable_mic_up.sh Update dot_config/scripts/executable_volume_up.sh

--- dot_config/scripts/executable_mic_up.sh
+++ dot_config/scripts/executable_mic_up.sh
@@ -3,12 +3,5 @@
 set -eu
 
 pactl set-source-mute @DEFAULT_SOURCE@ 0
-CURR_VOL="$(pactl list sources | grep '^\s*Volume' | awk '{print $5}' | sed 's/\%//' | tail -1)"
-if [ "$CURR_VOL" -lt 20 ] ; then
-    pactl set-source-volume @DEFAULT_SOURCE@ +5%
-fi
-CURR_VOL="$(pactl list sources | grep '^\s*Volume' | awk '{print $5}' | sed 's/\%//' | tail -1)"
-if [ "$CURR_VOL" -gt 20 ] ; then
-    pactl set-source-volume @DEFAULT_SOURCE@ 20%
-fi
+pactl set-source-volume @DEFAULT_SOURCE@ +5%
 
--- dot_config/scripts/executable_volume_up.sh
+++ dot_config/scripts/executable_volume_up.sh
@@ -3,12 +3,5 @@
 set -eu
 
 pactl set-sink-mute @DEFAULT_SINK@ 0
-CURR_VOL="$(pactl list sinks | grep '^\s*Volume' | awk '{print $5}' | sed 's/\%//' | tail -n1)"
-if [ "$CURR_VOL" -lt 100 ] ; then
-    pactl set-sink-volume @DEFAULT_SINK@ +5%
-fi
-CURR_VOL="$(pactl list sinks | grep '^\s*Volume' | awk '{print $5}' | sed 's/\%//' | tail -n1)"
-if [ "$CURR_VOL" -gt 100 ] ; then
-    pactl set-sink-volume @DEFAULT_SINK@ 100%
-fi
+pactl set-sink-volume @DEFAULT_SINK@ +5%
 
