a6eb7a1616498f08ea3cd0733a207a95b3383e1c
Adithya Nair <adtya@adtya.xyz>
2022-09-16 23:46:45 +0530

    Update dot_config/scripts/executable_mic_up.sh Update dot_config/scripts/executable_volume_up.sh

--- dot_config/scripts/executable_mic_up.sh
+++ dot_config/scripts/executable_mic_up.sh
@@ -4,6 +4,5 @@ set -eu
 
 wpctl set-mute @DEFAULT_AUDIO_SOURCE@ 0
 wpctl set-volume @DEFAULT_AUDIO_SOURCE@ 5%+
-if [ $(wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | awk -F': ' '{print $2}' | sed 's/\.//') -gt 10 ]; then
-  wpctl set-volume @DEFAULT_AUDIO_SOURCE@ 10%
-fi
+[ $(wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | awk -F': ' '{print $2}' | sed 's/\.//') -gt 10 ] && wpctl set-volume @DEFAULT_AUDIO_SOURCE@ 10%
+
--- dot_config/scripts/executable_volume_up.sh
+++ dot_config/scripts/executable_volume_up.sh
@@ -4,6 +4,5 @@ set -eu
 
 wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
 wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+
-if [ $(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk -F': ' '{print $2}' | sed 's/\.//') -gt 100 ]; then
-  wpctl set-volume @DEFAULT_AUDIO_SINK@ 100%
-fi
+[ $(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk -F': ' '{print $2}' | sed 's/\.//') -gt 100 ] && wpctl set-volume @DEFAULT_AUDIO_SINK@ 100%
+
