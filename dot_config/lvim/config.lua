8610f36379d061f29554b639333fdc3e087a5e87
Adithya Nair <adtya@adtya.xyz>
2022-09-24 00:40:01 +0530

    Update dot_config/lvim/config.lua

--- dot_config/lvim/config.lua
+++ dot_config/lvim/config.lua
@@ -15,31 +15,40 @@ lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
-lvim.builtin.nvimtree.show_icons.git = 0
+lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
   "bash",
-  "javascript",
   "json",
-  "lua",
-  "python",
+  "javascript",
   "typescript",
   "tsx",
+  "html",
   "css",
-  "yaml",
   "go",
-  "dockerfile"
+  "rust",
+  "yaml",
+  "toml",
+  "dockerfile",
+  "python",
+  "lua",
+  "markdown"
 }
 
 lvim.builtin.treesitter.ignore_install = { "haskell" }
 lvim.builtin.treesitter.highlight.enabled = true
 
 -- generic LSP settings
-lvim.lsp.automatic_servers_installation = false
+-- -- make sure server will always be installed even if the server is in skipped_servers list
+lvim.lsp.installer.setup.ensure_installed = {
+  "sumeko_lua",
+  "jsonls",
+  "gopls"
 
+}
 
 -- Additional Plugins
 lvim.plugins = {
-  { "dracula/vim" }
+  { "dracula/vim" },
 }
